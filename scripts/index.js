400b3bbb3309349ac0137e91d7df159db7ab0e0b
decideR <chimasterchimaster@gmail.com>
2023-05-20 15:11:19 +0700

    fix: card, profile, index.js, index.html

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,22 @@
 const popup = document.querySelector(".popup");
 const popupEdit = document.querySelector(".profile__edit-button");
 const popupClose = document.querySelector(".popup__close");
-const userName = popup.querySelector("#username");
-const userJob = popup.querySelector("#userjob");
+const userName = popup.querySelector(".popup__input_user_name");
+const userJob = popup.querySelector(".popup__input_user_job");
 const defaultUserName = document.querySelector(".profile__title");
 const defaultUserJob = document.querySelector(".profile__subtitle");
 const formElement = popup.querySelector(".popup__form");
+
+
+function openPopup(attribute) {
+  attribute.classList.add("popup__opened");
+  userName.value = defaultUserName.textContent;
+  userJob.value = defaultUserJob.textContent;
+};
+
+function closePopup(attribute) {
+  attribute.classList.remove("popup__opened");
+};
 
 
 popupEdit.addEventListener("click", function() {
@@ -16,20 +27,9 @@ popupClose.addEventListener("click", function() {
   closePopup(popup);
 });
 
-userName.value = defaultUserName.textContent;
-userJob.value = defaultUserJob.textContent;
-
 formElement.addEventListener("submit", function(event) {
   event.preventDefault();
   defaultUserName.textContent = userName.value;
   defaultUserJob.textContent = userJob.value;
   closePopup(popup);
 });
-
-function openPopup(attribute) {
-  attribute.classList.add("popup__opened");
-};
-
-function closePopup(attribute) {
-  attribute.classList.remove("popup__opened");
-};
