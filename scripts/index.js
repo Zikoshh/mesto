4bbe19bf4e850b3894420a7a0df2c4c0a3d044db
decideR <chimasterchimaster@gmail.com>
2023-06-01 12:30:51 +0700

    fix: delete initial-cards.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-/*popupImage*/
+/*imagePopup*/
 const imagePopup = document.querySelector(".popup_image")
 const imagePopupTitle = imagePopup.querySelector(".popup__title_image");
 const imagePopupPicture = imagePopup.querySelector(".popup__image");
@@ -23,6 +23,32 @@ const cardTemplate = document.querySelector(".card-template");
 const cardTemplateContent = cardTemplate.content;
 const card = cardTemplateContent.querySelector(".card");
 const cardsContainer = document.querySelector(".cards");
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
 
 
 
